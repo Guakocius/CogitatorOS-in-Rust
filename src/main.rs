dc280d43ad9d7c6a7dc9e0b9c7224e70c8e44658
Guakocius <engelhardt.alexander@mein.gmx>
2025-10-22 18:33:04 +0200

    Fully implemented VGA buffer with print and println macros and a working panic handler with println

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+mod vga_buffer;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! { 
+    println!("Finally! Hello, world{}", "!");
+    loop {}
+}
